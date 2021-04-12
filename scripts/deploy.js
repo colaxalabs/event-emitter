65d0b80f5b38971bd5405a2af885718ef77ba5ba
3dw1nM0535 <workockmoses@gmail.com>
2021-04-12 16:23:22 +0300

    chore(deployment)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,17 @@
-/* global task */
+/* global task, ethers */
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("deploy:kovan", "Deploy contract(s) to Kovan testnet").setAction(
-  async (hre) => {
+  async () => {
     // get signer
-    const [deployer] = await hre.ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
     console.log("Deployer: ", await deployer.getAddress());
 
     // deploy counter address
-    const counter = await (await hre.ethers.getContractFactory("Counter"))
+    const counter = await (await ethers.getContractFactory("Counter"))
       .connect(deployer)
       .deploy();
     console.log("Counter deployed at: ", counter.address);
